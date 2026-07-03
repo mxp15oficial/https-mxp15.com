b4504f91cb0be4ad150892d75b1c742f9d9be7e0
poltroniericorreiam-code <poltroniericorreiam@gmail.com>
2026-07-03 14:26:58 -0400

    Refactor navigation functions in script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function entrarJogo(){
+function entrar() {
+    window.location.href = "Página inicial.html";
+}
+
+function cadastrar() {
+    window.location.href = "cadastro.html";
+}
+
+function entrarJogo() {
     window.location.href = "jogo.html";
 }
