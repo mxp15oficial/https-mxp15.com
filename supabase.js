efb36a586da507e5510f58747d8164d731419dc0
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 17:54:15 -0400

    Update Supabase configuration and client initialization

--- supabase.js
+++ supabase.js
@@ -1,6 +1,9 @@
-// supabase.js
-const SUPABASE_URL = "SUA_URL_AQUI";
-const SUPABASE_ANON_KEY = "SUA_CHAVE_AQUI";
+const SUPABASE_URL = "https://guavczqfnqkshhgpyccs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_1qUbWglTgDfCV4MIDT7qWQ_jetIG_ht";
 
-// Criação correta do cliente para a v2 da biblioteca
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+alert("Supabase conectado!");
