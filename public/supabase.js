fbadf72f171a5bf00c34ff1bfd0a7d4dfc0bc796
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 19:22:21 -0400

    Refactor Supabase client initialization

--- public/supabase.js
+++ public/supabase.js
@@ -2,8 +2,6 @@ const SUPABASE_URL = "https://guavczqfnqkshhgpyccs.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_1qUbWglTgDfCV4MIDT7qWQ_jetIG_ht";
 
 const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
-
-alert("Supabase conectado!");
