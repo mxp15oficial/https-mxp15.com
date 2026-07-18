b10237b79bac4248de1d5f5f7763b0b55ef502bb
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 16:44:42 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-// supabase.js
+ supabase.js
 const SUPABASE_URL = "SUA_URL_AQUI";
 const SUPABASE_ANON_KEY = "SUA_CHAVE_AQUI";
 
-// Inicialização correta
-window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Criação correta do cliente para a v2 da biblioteca
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
