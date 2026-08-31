fe629a80e33f917da0523ead40da1bd423a466a1
mxp15oficial <poltroniericorreiam@gmail.com>
2026-08-31 09:24:26 -0400

    Update URLs in config.js
    Replaced old URLs with new mxp URLs in the config.

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 {
   "url": [
-    "kk.w9t9u2.com",
-    "kk.e7u5k9.com",
-    "kk.r3p8p7.com",
-    "kk.j9o8r5.com",
-    "kk.s6f6q8.com",
-    "kk.e9a8b2.com",
-    "kk.g6p8y5.com",
-    "kk.m4g3r1.com",
-    "kk.z1h2j2.com",
-    "kk.a6m6h5.com"
+    "mxp.w9t9u2.com",
+    "mxp.e7u5k9.com",
+    "mxp.r3p8p7.com",
+    "mxp.j9o8r5.com",
+    "mxp.s6f6q8.com",
+    "mxp.e9a8b2.com",
+    "mxp.g6p8y5.com",
+    "mxp.m4g3r1.com",
+    "mxp.z1h2j2.com",
+    "mxp.a6m6h5.com"
   ],
   "domainParams": "sd=3",
   "headers": {
