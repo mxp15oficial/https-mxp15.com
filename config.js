35f2f9ff83f7fa14cd5a51c5f9bce1bd8fbe6e29
mxp15oficial <poltroniericorreiam@gmail.com>
2026-08-31 09:21:33 -0400

    Add configuration file with URLs and headers

---
+++ config.js
@@ -0,0 +1,20 @@
+{
+  "url": [
+    "kk.w9t9u2.com",
+    "kk.e7u5k9.com",
+    "kk.r3p8p7.com",
+    "kk.j9o8r5.com",
+    "kk.s6f6q8.com",
+    "kk.e9a8b2.com",
+    "kk.g6p8y5.com",
+    "kk.m4g3r1.com",
+    "kk.z1h2j2.com",
+    "kk.a6m6h5.com"
+  ],
+  "domainParams": "sd=3",
+  "headers": {
+    "X-Dev": false,
+    "X-Gray": false,
+    "Content-Type": "application/json"
+  }
+}
